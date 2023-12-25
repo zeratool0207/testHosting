fbbdfd45f0cdf80a4c0ac59513806ca8c9b50660
zeratool0207 <zeratool0207@naver.com>
2023-12-25 21:24:21 +0900

    initial commit ...

---
+++ js/movieList.js
@@ -0,0 +1,97 @@
+
+//<!-- 상단 전체 영화 콤보박스 DATA -->
+// 유튜브 영화 LIST 정보 
+
+
+var movieList = 
+
+{
+	"talaile" : [
+        {
+            "name" :"타래료 칭폐안",
+            "episode" : 1,
+            "url" : "SjCcXmqO8GE"
+        }, 
+        {
+            "name" :"타래료 칭폐안",
+            "episode" : 2,
+            "url" : "ha1TJPLuBdY"
+        }, 
+        {
+            "name" :"타래료 칭폐안",
+            "episode" : 3,
+            "url" : "OOat7kv36Pg"
+        }, 
+        {
+            "name" :"타래료 칭폐안",
+            "episode" : 4,
+            "url" : "rk5j5gQ8UNU"
+        }, 
+        {
+            "name" :"타래료 칭폐안",
+            "episode" : 5,
+            "url" : "5cM5qzZRkAc"
+        }, 
+        {
+            "name" :"타래료 칭폐안",
+            "episode" : 6,
+            "url" : "hLiDPNs0ADk"
+        }, 
+        {
+            "name" :"타래료 칭폐안",
+            "episode" : 7,
+            "url" : "YdUBMaSTGQA"
+        }, 
+        {
+            "name" :"타래료 칭폐안",
+            "episode" : 9,
+            "url" : "OuxmeMc_ZJM"
+        }, 
+        {
+            "name" :"타래료 칭폐안",
+            "episode" : 10,
+            "url" : "aWcjtjXmA5w"
+        } 
+    ],
+
+	"wodemimi" : [
+        {
+            "name" :"나의 비밀친구",
+            "episode" : 1,
+            "url" : "L8fqygGnnhE"
+        },
+        {
+            "name" :"나의 비밀친구",
+            "episode" : 2,
+            "url" : "kHEV-SiXB9g"
+        }
+    ],
+
+	"tadecheng" : [
+        {
+            "name" :"그녀의 세계",
+            "episode" : 1,
+            "url" : "aGNk0OLln5A"
+        },
+        {
+            "name" :"그녀의 세계",
+            "episode" : 2,
+            "url" : "ztB2xnOnjN4"
+        },
+        {
+            "name" :"그녀의 세계",
+            "episode" : 3,
+            "url" : "dcfxzD32z3c"
+        }
+    ],
+
+    "zhixianghe" : [
+        {
+            "name" :"보고싶어",
+            "episode" : 1,
+            "url" : "-CWFebE6fYg"
+        }
+    ]    
+
+
+}
